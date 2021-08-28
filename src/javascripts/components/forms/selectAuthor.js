9946c5b8095434eaa869cbc0526e8140fc0cd792
Daniel Sitarek <deltasierra777@gmail.com>
2021-08-28 15:09:03 -0500

    update functionality

--- src/javascripts/components/forms/selectAuthor.js
+++ src/javascripts/components/forms/selectAuthor.js
@@ -1,13 +1,13 @@
 import { getAuthors } from '../../helpers/data/authorData';
 
-const selectAuthor = () => {
+const selectAuthor = (id) => {
   let domString = `<label for="author">Select an Author</label>
     <select class="form-control" id="author" required>
-    <option value="">Select an Author</option>`;
+    <option value="${id}">Select an Author</option>`;
 
   getAuthors().then((authorsArray) => {
     authorsArray.forEach((author) => {
-      domString += `<option value="${author.firebaseKey}">${author.first_name} ${author.last_name}</option>`;
+      domString += `<option value="${author.firebaseKey}" ${id === author.firebaseKey ? 'selected' : ''}>${author.first_name} ${author.last_name}</option>`;
     });
 
     domString += '</select>';
